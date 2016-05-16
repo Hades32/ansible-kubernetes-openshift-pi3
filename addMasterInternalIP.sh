fdaa35a23302430f58916d66654a419e7d7fd48c
Martin Rauscher <hades32@gmail.com>
2016-05-16 15:53:13 +0000

    delete obsolete file

--- addMasterInternalIP.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-masterip=$(./getMasterInternalIP.sh)
-./getServerIPs.sh | xargs -n1 -I{} ssh -q -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -l root {} sh -c "'echo $masterip mast er >> /etc/hosts'"
